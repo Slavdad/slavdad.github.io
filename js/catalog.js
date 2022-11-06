b4204e9e663901637d820ac19b7d4afee11706f2
Vlad <60463357+Slavdad@users.noreply.github.com>
2022-11-06 13:40:58 +0600

    my dont end pjogecr

---
+++ js/catalog.js
@@ -0,0 +1,116 @@
+$('.menu_item_contacts').on('click', function(e) {
+    e.preventDefault();
+    return $("html, footer").animate({
+      scrollTop: $("#footer_rect_navigation").offset().top
+    });
+  });
+
+  let condition = true;
+  let start = 0;
+  let end = 0;
+  
+  // Анимация меню
+  function forwards() {
+      anime({
+       targets: '.menu-small',
+       translateX: ['-100%', '0'],
+       easing: 'easeInOutQuad',
+       direction: 'alternate',
+       duration: 1000,
+       loop: false
+      });
+      condition: false;
+     }
+     
+  function backwards() {
+      anime({
+       targets: '.menu-small',
+       translateX: ['0', '-100%'],
+       easing: 'easeInOutQuad',
+       direction: 'alternate',
+       duration: 1000,
+       loop: false
+      });
+      condition: true;
+     }
+  
+  // Анимация "Бургера"
+  function stick_1(){
+      anime({
+          targets: '.menu_small.icon',
+          rotate: 90,
+          easing: 'easeInOutQuad',
+          direction: 'alternate',
+          duration: 1000,
+          loop: false
+         })
+  
+      anime({
+          targets: '.stick',
+          rotate: 180,
+          easing: 'easeInOutQuad',
+          direction: 'alternate',
+          duration: 1000,
+          loop: false
+     })
+     }
+  
+  function stick_2(){
+      anime({
+          targets: '.menu_small.icon',
+          rotate: 90,
+          easing: 'easeInOutQuad',
+          direction: 'alternate',
+          duration: 1000,
+          loop: false
+         })
+  
+      anime({
+          targets: '.stick',
+          rotate: 360,
+          easing: 'easeInOutQuad',
+          direction: 'alternate',
+          duration: 1000,
+          loop: false
+     })
+     }
+  
+     $('.personages').on('click', function(e) {
+        e.preventDefault();
+        return $("html, body").animate({
+          scrollTop: $("#catalog-navigation").offset().top
+        });
+    });
+
+  $('.menu_small_icon').click(function () {
+      if (condition == true) {
+          forwards();
+          stick_1();
+       // изменение значения переменной condition
+          condition = false
+      } else {
+       // анимация скрытия
+          backwards();
+          stick_2();
+       // изменение значения переменной condition
+          condition = true
+      }
+  });
+     
+  $('.container').on('touchstart', function(event){
+      start = event.originalEvent.touches[0].pageX;
+  })
+  $('.container').on('touchend', function(event){
+      end = event.originalEvent.changedTouches[0].pageX;
+      if (end - start >= 100 && condition) {
+          forwards();
+          condition = false;
+      }
+      else if (start - end >= 100 && !condition) {
+          backwards();
+          condition = true;
+      }
+  })
+
+
+
---
+++ js/script.js
@@ -0,0 +1,148 @@
+let condition = true;
+let start = 0;
+let end = 0;
+
+// Анимация меню
+function forwards() {
+    anime({
+     targets: '.menu-small',
+     translateX: ['-100%', '0'],
+     easing: 'easeInOutQuad',
+     direction: 'alternate',
+     duration: 1000,
+     loop: false
+    });
+    condition: false;
+   }
+   
+function backwards() {
+    anime({
+     targets: '.menu-small',
+     translateX: ['0', '-100%'],
+     easing: 'easeInOutQuad',
+     direction: 'alternate',
+     duration: 1000,
+     loop: false
+    });
+    condition: true;
+   }
+
+// Анимация "Бургера"
+function stick_1(){
+    anime({
+        targets: '.menu_small.icon',
+        rotate: 90,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+       })
+
+    anime({
+        targets: '.stick',
+        rotate: 180,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+   })
+   }
+
+function stick_2(){
+    anime({
+        targets: '.menu_small.icon',
+        rotate: 90,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+       })
+
+    anime({
+        targets: '.stick',
+        rotate: 360,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+   })
+   }
+
+
+$('.menu_small_icon').on('click' ,function () {
+
+    if (condition == true) {
+        forwards();
+        stick_1();
+     // изменение значения переменной condition
+        condition = false
+    } else {
+     // анимация скрытия
+        backwards();
+        stick_2();
+     // изменение значения переменной condition
+        condition = true
+    }
+});
+   
+$('.container').on('touchstart', function(event){
+    start = event.originalEvent.touches[0].pageX;
+})
+$('.container').on('touchend', function(event){
+    end = event.originalEvent.changedTouches[0].pageX;
+    if (end - start >= 100 && condition) {
+        forwards();
+        condition = false;
+    }
+    else if (start - end >= 100 && !condition) {
+        backwards();
+        condition = true;
+    }
+})
+
+$('.arrow').on('click', function(e) {
+    e.preventDefault();
+    return $("html, body").animate({
+      scrollTop: $("#rectangle").offset().top
+    });
+  });  
+
+$('.menu_item_contacts').on('click', function(e) {
+    e.preventDefault();
+    return $("html, footer").animate({
+      scrollTop: $("#footer_rect_navigation").offset().top
+    });
+});
+
+$(document).ready(function(){
+  $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      autoplay: true,
+      autoplaySpeed: 1400,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            // centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+});
+
+
+
